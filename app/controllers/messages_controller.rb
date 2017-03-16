7db98bb13e497cea6896702e3f7abcd39c5f9f30
huyphung1602 <huy.phung.sw@gmail.com>
2017-03-16 13:03:28 +0700

    Finish milestone 4 - broadcast new messages to all clients in MessagesController#create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,6 +8,7 @@ class MessagesController < ApplicationController
 
     if @message.save
       flash[:success] = "Message sent successfully"
+      ActionCable.server.broadcast("my_channel", message: render_message(@message))
       redirect_to root_path
     else
       flash[:error] = "Error #{@message.errors.full_mesages.to_sentence}"
@@ -16,6 +17,11 @@ class MessagesController < ApplicationController
   end
 
   private
+    def render_message(message)
+      ApplicationController.render(partial: 'messages/message',
+                                   locals: {message: message})
+    end
+
     def message_params
       params.require(:message).permit(:body)
     end
